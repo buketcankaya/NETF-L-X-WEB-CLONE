d465c2d9715681185495d1fbad5df06e4cbafe70
BUKET CAN KAYA <bkt.can@gmail.com>
2023-11-26 15:14:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# kullanılan kütüphaneler
+
+- react-router-dom
+- @splidejs/react-splide
+- axios
+- redux
+- react-redux
+- redux-thunk
+- millify
+- react splice
+
+# kaynaklar
+
+Film API : https://www.themoviedb.org/
+Resim Base URL: http://image.tmdb.org/t/p/original
+
+
+![](../screen.gif)# netfilix-clone
---
+++ src/redux/movieReducer.js
@@ -0,0 +1,33 @@
+import { actionTypes } from "../redux/actionTypes";
+
+const initialState = {
+  populerMovies: [],
+  genres: [],
+  isLoading: true,
+};
+
+export const movieReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_MOVIES:
+      //console.log('Redux Log',action.payload.results)
+      return {
+        ...state,
+        populerMovies: action.payload.results,
+        isLoading: false,
+      };
+
+    case actionTypes.SET_LOADING:
+      return {
+        isLoading: action.payload,
+      };
+
+    case actionTypes.SET_CATEGORIES:
+      return {
+        ...state,
+        genres: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
